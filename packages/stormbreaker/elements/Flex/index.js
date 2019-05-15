901acedd0793fe498bd168b1f608fca57bb83eeb
Anoop Raveendran <anooprav7@gmail.com>
2019-05-15 16:50:22 +0530

    added Readme and stuff

--- packages/stormbreaker/elements/Flex/index.js
+++ packages/stormbreaker/elements/Flex/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import Box from '../Box';
-import { flexWrap, flexDirection, alignItems, justifyContent } from 'styled-system';
+import { flexWrap, flexDirection, alignItems, justifyContent, alignContent } from 'styled-system';
 import styled from 'styled-components';
 
-const Flex = styled(Box)(
+const StyledFlex = styled(Box)(
 	{
 		display: 'flex'
 	},
 	flexWrap,
 	flexDirection,
 	alignItems,
+	alignContent,
 	justifyContent
 );
+
+function Flex(props) {
+	const { children, remainingProps } = props;
+	return <StyledFlex {...remainingProps}>{children}</StyledFlex>;
+}
 
 export default Flex;
 
@@ -19,5 +25,6 @@ Flex.propTypes = {
 	...flexWrap.propTypes,
 	...flexDirection.propTypes,
 	...alignItems.propTypes,
-	...justifyContent.propTypes
+	...justifyContent.propTypes,
+	...alignContent.propTypes
 };
